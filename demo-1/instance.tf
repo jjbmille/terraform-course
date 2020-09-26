d2e4848c2869adfec4ad93c37e9816df964266ca
Jeff Miller <jjbmille@gmail.com>
2020-09-26 17:28:22 -0400

    updating demo1

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -1,5 +1,8 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "demo1-savage-instance" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
+  tags = {
+    Name = "savage-instance"
+  }
 }
 
--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-0f82752aa17ff8f5d"
   instance_type = "t2.micro"
 }
 
